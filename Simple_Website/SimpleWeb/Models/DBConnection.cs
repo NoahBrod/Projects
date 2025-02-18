8681bd5e9053bcf3a9566409c6812659e9284e59
Noah Brodke <noahbrodke@gmail.com>
2025-02-18 02:51:22 -0500

    Tried .net

---
+++ Simple_Website/SimpleWeb/Models/DBConnection.cs
@@ -0,0 +1,41 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace SimpleWeb.Models
+{
+	public class DBConnection
+	{
+		private DBConnection()
+		{
+
+		}
+
+		public String Server { get; set; }
+		public string DatabaseName { get; set; }
+		public string Username { get; set; }
+		public string Password { get; set; }
+
+		public MySqlConnection Connection { get; set; }
+
+		private static DBConnection _instance = null;
+		public static DBConnection Instance() {
+			if (_instance == null)
+				_instance = new DBConnection();
+			return _instance;
+		}
+
+		public bool IsConnect() {
+			if (Connection == null) {
+				if (String.IsNullOrEmpty(DatabaseName))
+					return false;
+				string connString = string.Format("Server=127.0.0.1;uid=root;pwd=root;database=simpleweb");
+				Connection = new MySqlConnection(connString);
+				Connection.Open();
+			}
+			return true;
+		}
+
+		public void close() {
+			Connection.Close();
+		}
+	}
+}
---
+++ Simple_Website/SimpleWeb/wwwroot/js/site.js
@@ -0,0 +1,6 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+$(document).ready(function() {
+    console.log('LOADED');
+});
+// Write your JavaScript code.
